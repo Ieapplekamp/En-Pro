2bc067d9ad8b8ce5dd2dce09017f879442275159
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-12 12:16:44 -0400

    particles.js is working

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,1 +1,7 @@
 console.log('whuddup bitch');
+// BIIIITTCHHHH
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'Assets/Javascript/particles.json', function() {
+    console.log('callback - particles-js config loaded');
+  });
+  
