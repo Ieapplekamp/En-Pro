d6ca2068852875797787fb84a3f8bc83d5940da9
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-24 21:03:03 -0400

    test 5

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,5 +1,5 @@
 // /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS('particles-js', 'Assets/Javascript/particles.json', function() {
+particlesJS.load('particles-js', 'Assets/Javascript/particles.json', function() {
     console.log('callback - particles-js config loaded');
   });
   
