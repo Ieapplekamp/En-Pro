282b0d0183ed9f6ee8c199472839c69e123ecf08
Ian Applekamp <ieapplekamp@gmail.com>
2019-10-28 02:35:10 -0400

    stupid fucking particles needs to be aligned correctly

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,5 +1,9 @@
 // /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'Assets/Javascript/particles.json', function() {
+particlesJS.load('particles-js', 'Assets/Javascript/particles1.json', function() {
     console.log('callback - particles-js config loaded');
-  });
+});
+  
+particlesJS.load('particles-js2', 'Assets/Javascript/particles2.json', function() {
+  console.log('callback - particles-js2 config loaded');
+});
   
