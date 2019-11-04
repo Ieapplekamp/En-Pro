15c2558bda72b9878dcabf8d838beea4df0b3d9e
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-04 16:35:17 -0500

    Do not care for the way this looks at all

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,3 +1,4 @@
+
 // /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'Assets/Javascript/particles1.json', function() {
     console.log('callback - particles-js config loaded');
@@ -6,4 +7,11 @@ particlesJS.load('particles-js', 'Assets/Javascript/particles1.json', function()
 particlesJS.load('particles-js2', 'Assets/Javascript/particles2.json', function() {
   console.log('callback - particles-js2 config loaded');
 });
+
+//  AOS causes particles to not work
+
+// import AOS from 'aos';
+// import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+// AOS.init();
   
