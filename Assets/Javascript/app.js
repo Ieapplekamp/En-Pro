dc1546784295089d1724d6b54e196febb1411745
Ian Applekamp <ieapplekamp@gmail.com>
2019-11-28 21:21:03 -0500

    almost full mobile responsive

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,5 +1,4 @@
 
-// /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'Assets/Javascript/particles1.json', function() {
     console.log('callback - particles-js config loaded');
 });
@@ -8,10 +7,4 @@ particlesJS.load('particles-js2', 'Assets/Javascript/particles2.json', function(
   console.log('callback - particles-js2 config loaded');
 });
 
-//  AOS causes particles to not work
-
-// import AOS from 'aos';
-// import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
-// AOS.init();
   
